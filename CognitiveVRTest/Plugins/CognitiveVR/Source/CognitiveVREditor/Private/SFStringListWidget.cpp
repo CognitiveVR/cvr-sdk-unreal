0349cac27981fe0a58f889f92e3f0cfda0e70c9f
calderarchinuk <calderarchinuk@gmail.com>
2017-12-27 10:42:48 -0800

    added missing changes to list widget

--- CognitiveVRTest/Plugins/CognitiveVR/Source/CognitiveVREditor/Private/SFStringListWidget.cpp
+++ CognitiveVRTest/Plugins/CognitiveVR/Source/CognitiveVREditor/Private/SFStringListWidget.cpp
@@ -3,61 +3,22 @@
 
 void SFStringListWidget::Construct(const FArguments& Args)
 {
-	//DelimiterImage = InArgs._DelimiterImage;
-	//Items = Args._DynamicData;
-
 	ChildSlot
 		[
 			SNew(SVerticalBox)
-			/*+SVerticalBox::Slot()
-			.AutoHeight()
-			[
-				SNew(SBox)
-				[
-					SNew(SButton)
-					.Text(FText::FromString("Refresh"))
-					.OnClicked(this, &SFStringListWidget::ButtonPressed)
-				]
-			]*/
 			+SVerticalBox::Slot()
 			.AutoHeight()
+			.HAlign(EHorizontalAlignment::HAlign_Center)
 			[
 				SNew(STextBlock)
 				.Text(FText::FromString("Sub Directories"))
 			]
 			+SVerticalBox::Slot()
-			//.FillHeight(2)
 			[
 				SAssignNew(ListViewWidget, SListView<TSharedPtr<FString>>)
 				.ItemHeight(24)
 				.ListItemsSource(&SubDirectoryNames) //The Items array is the source of this listview
 				.OnGenerateRow(this, &SFStringListWidget::OnGenerateRowForList)
-				/*.HeaderRow(
-					SNew(SHeaderRow)
-					+ SHeaderRow::Column("name")
-					.FillWidth(1)
-					[
-						SNew(STextBlock)
-						//.MinDesiredWidth(256)
-						.Text(FText::FromString("Name"))
-					]
-
-					+ SHeaderRow::Column("mesh")
-					.FillWidth(1)
-					[
-						SNew(STextBlock)
-						//.MinDesiredWidth(256)
-						.Text(FText::FromString("MeshName"))
-					]
-
-					+ SHeaderRow::Column("id")
-					.FillWidth(0.3)
-					[
-						SNew(STextBlock)
-						//.MinDesiredWidth(512)
-						.Text(FText::FromString("Id"))
-					]
-				)*/
 			]
 		];
 }
