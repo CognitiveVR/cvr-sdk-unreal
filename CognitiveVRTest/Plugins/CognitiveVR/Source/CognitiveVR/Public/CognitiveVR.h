07be018b80479326e8bad6fef17def3c8ffa85e6
Alfred Shaker <ashaker@kent.edu>
2023-11-06 14:51:54 -0500

    version number update

--- CognitiveVRTest/Plugins/CognitiveVR/Source/CognitiveVR/Public/CognitiveVR.h
+++ CognitiveVRTest/Plugins/CognitiveVR/Source/CognitiveVR/Public/CognitiveVR.h
@@ -13,7 +13,7 @@
 DEFINE_LOG_CATEGORY_STATIC(CognitiveVR_Log, Log, All);
 
 #define COGNITIVEVR_SDK_NAME "unreal"
-#define COGNITIVEVR_SDK_VERSION "0.26.1"
+#define COGNITIVEVR_SDK_VERSION "0.27.0"
 
 class IAnalyticsProvider;
 class FAnalyticsProviderCognitiveVR;
