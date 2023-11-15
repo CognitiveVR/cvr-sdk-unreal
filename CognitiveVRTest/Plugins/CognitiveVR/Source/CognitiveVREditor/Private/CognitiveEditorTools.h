0458e70c7e986959404948c3e07da012781ede45
Alfred Shaker <ashaker@kent.edu>
2023-11-15 13:02:53 -0500

    dynamic object export rework
    this rework makes it so that actors and pawns can be exported, with one or multiple dynamic objects. multiple dynamic objects have to be a child of each mesh they are on when on one actor.

--- CognitiveVRTest/Plugins/CognitiveVR/Source/CognitiveVREditor/Private/CognitiveEditorTools.h
+++ CognitiveVRTest/Plugins/CognitiveVR/Source/CognitiveVREditor/Private/CognitiveEditorTools.h
@@ -164,18 +164,18 @@ public:
 	void UploadFromDirectory(FString url, FString directory, FString expectedResponseType);
 
 	//dynamic objects
-	//Runs the built-in fbx exporter with all meshes
+	//Runs the built-in gltf exporter with all meshes
 	FProcHandle ExportAllDynamics();
-	//Runs the built-in fbx exporter with all meshes that don't have an exported .gltf
+	//Runs the built-in gltf exporter with all meshes that don't have an exported .gltf
 		FProcHandle ExportNewDynamics();
 
-	//Runs the built-in fbx exporter with selected meshes
+	//Runs the built-in gltf exporter with selected meshes
 		FReply ExportSelectedDynamics();
 		FProcHandle ExportDynamicData(TArray< TSharedPtr<FDynamicData>> dynamicData);
 	
 
 	//this is the important function for exporting dynamics. all other other dynamic export functions lead to this
-		//sets position to origin, export as fbx, generate screenshot, bake materials to textures (not necessary)
+		//sets position to origin, export as gltf, generate screenshot
 	FProcHandle ExportDynamicObjectArray(TArray<UDynamicObject*> exportObjects);
 
 
