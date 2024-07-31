71fd38caabd0f2b86f73b85b47e931df70905674
Alfred Shaker <ashaker@kent.edu>
2024-07-31 19:34:44 -0400

    added more functions for sdks, adjusted formatting

--- Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Cognitive3D.Build.cs
+++ Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Cognitive3D.Build.cs
@@ -6,12 +6,50 @@ namespace UnrealBuildTool.Rules
 {
 	public class Cognitive3D : ModuleRules
 	{
+		private string pluginsDirectory;
+
 		public Cognitive3D(ReadOnlyTargetRules Target): base(Target)
 		{
 			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 			bLegacyPublicIncludePaths = true;
-			
-            PublicIncludePathModuleNames.AddRange(
+
+			PublicDependencyModuleNames.Add("DeveloperSettings");
+			pluginsDirectory = System.IO.Path.Combine(Target.ProjectFile.Directory.ToString(), "Plugins");
+
+			//uncomment the following line to enable Oculus/Meta platform functionality. Uses OculusVR for UE4 and OculusXR (MetaXR) for UE5.
+			//MetaXRPlugin();
+
+			//Uncomment the following line to enable Oculus Passthrough features. UE 5.2 onward (MUST ALSO ENABLE OCULUS PLUGIN ABOVE)
+			//MetaXRPassthrough();
+
+			//Uncomment the following line to enable Oculus Platform features
+			//MetaXRPlatform();
+
+			//Uncomment the following line to enable PICOXR SDK features
+			//PICOXR();
+
+			//Uncomment the following line to enable eye tracking support using IEyeTracker interface (varjo openxr support, etc)
+			//OpenXREyeTracking();
+
+			//Uncomment the following line to enable Vive WaveVR eye tracking support
+			//WaveVREyeTracking();
+
+			//Varjo (up to and including version 3.0.0)
+			//void Varjo();
+
+			//TobiiEyeTracking
+			//void TobiiEyeTracking();
+
+			//Vive Pro Eye (SRanipal)
+			//void SRapnipalVivePro();
+
+			//Pico Neo 2 Eye
+			//void PicoMobile();
+
+			//HP Omnicept
+			//void HPGlia();
+
+			PublicIncludePathModuleNames.AddRange(
                 new string[] {
                     "Core",
                     "CoreUObject",
@@ -58,134 +96,26 @@ namespace UnrealBuildTool.Rules
 				}
 				);
 
-			PublicDependencyModuleNames.Add("DeveloperSettings");
-
-			//uncomment the following line to enable Oculus/Meta platform functionality. Uses OculusVR for UE4 and OculusXR (MetaXR) for UE5.
-			//MetaXRPlugin();
-
-			//Uncomment the following line to enable Oculus Passthrough features. UE 5.2 onward (MUST ALSO ENABLE OCULUS PLUGIN ABOVE)
-			//MetaXRPassthrough();
-
-			//Uncomment the following line to enable Oculus Platform features
-			//MetaXRPlatform();
-
-			//Uncomment the following line to enable PICOXR SDK features
-			//PICOXR();
-
-			//Uncomment the following line to enable eye tracking support using IEyeTracker interface (varjo openxr support, etc)
-			//OpenXREyeTracking();
-
-
-			//Uncomment the following line to enable Vive WaveVR eye tracking support
-			//WaveVREyeTracking();
-
-			var pluginsDirectory = System.IO.Path.Combine(Target.ProjectFile.Directory.ToString(),"Plugins");
-		
-		//Varjo (up to and including version 3.0.0)
-		if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory,"Varjo")))
-		{
-			System.Console.WriteLine("Cognitive3D.Build.cs found Varjo Plugin folder");
-			PublicDependencyModuleNames.Add("VarjoHMD");
-			PublicDependencyModuleNames.Add("VarjoEyeTracker");
-		}
-		
-		//TobiiEyeTracking
-		if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory,"TobiiEyeTracking")))
-		{
-			System.Console.WriteLine("Cognitive3D.Build.cs found TobiiEyeTracking Plugin folder");
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					"../../TobiiEyeTracking/Source/TobiiCore/Private",
-					"../../TobiiEyeTracking/Source/TobiiCore/Public"
-				});
-
-			PublicDependencyModuleNames.Add("TobiiCore");
-		}
-		
-		//Vive Pro Eye (SRanipal)
-		if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory,"SRanipal"))) 
-		{
-			//ideally read the uplugin file as json and get the VersionName
-			//for now, just read the source directory layout
-			var sranipalPlugin = System.IO.Path.Combine(pluginsDirectory,"SRanipal");
-			var sranipalSource = System.IO.Path.Combine(sranipalPlugin,"Source");
-			string[] sourceModules = System.IO.Directory.GetDirectories(sranipalSource);
-			if (sourceModules.Length == 2)//1.1.0.1 and 1.2.0.1 only have eye and lip modules
-			{
-				PublicDefinitions.Add("SRANIPAL_1_2_API");
-				System.Console.WriteLine("Cognitive3D.Build.cs found SRanipal Plugin folder");
-				PrivateIncludePaths.AddRange(
-					new string[] {
-						"../../SRanipal/Source/SRanipal/Private",
-						"../../SRanipal/Source/SRanipal/Public"
-					});
-
-				PublicDependencyModuleNames.Add("SRanipal");
-
-				string BaseDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(ModuleDirectory, "..", "..", ".."));
-				string SRanipalDir = System.IO.Path.Combine(BaseDirectory,"SRanipal","Binaries",Target.Platform.ToString());
-				PublicAdditionalLibraries.Add(System.IO.Path.Combine(SRanipalDir,"SRanipal.lib"));
-				PublicDelayLoadDLLs.Add(System.IO.Path.Combine(SRanipalDir,"SRanipal.dll"));	
-			}
-			else if (sourceModules.Length == 5)
-			{
-				PublicDefinitions.Add("SRANIPAL_1_3_API");
-				System.Console.WriteLine("Cognitive3D.Build.cs found SRanipal Plugin folder 1.3.0.9 or newer");
-				PrivateIncludePaths.AddRange(
-					new string[] {
-						"../../SRanipal/Source/SRanipal/Private",
-						"../../SRanipal/Source/SRanipal/Public",
-						"../../SRanipal/Source/SRanipalEye/Private",
-						"../../SRanipal/Source/SRanipalEye/Public",
-						"../../SRanipal/Source/SRanipalEyeTracker/Private",
-						"../../SRanipal/Source/SRanipalEyeTracker/Public"
-					});
-
-				PublicDependencyModuleNames.Add("SRanipal");
-				PublicDependencyModuleNames.Add("SRanipalEye");
-				PublicDependencyModuleNames.Add("SRanipalEyeTracker");
-
-				string BaseDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(ModuleDirectory, "..", "..", ".."));
-				string SRanipalDir = System.IO.Path.Combine(BaseDirectory,"SRanipal","Binaries",Target.Platform.ToString());
-				PublicAdditionalLibraries.Add(System.IO.Path.Combine(SRanipalDir,"SRanipal.lib"));
-				PublicDelayLoadDLLs.Add(System.IO.Path.Combine(SRanipalDir,"SRanipal.dll"));	
-			}
-		}
-
-		//Pico Neo 2 Eye
-		if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory,"PicoMobile")))
-		{
-			System.Console.WriteLine("Cognitive3D.Build.cs found Pico Plugin folder");
-			PublicDependencyModuleNames.Add("PicoMobile");
-		}
-
-        //HP Omnicept
-		if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory, "HPGlia")))
-		{
-			System.Console.WriteLine("Cognitive3D.Build.cs found HP Glia Omnicept folder");
-			PublicDependencyModuleNames.Add("HPGlia");
-		}
-
-		//this is all for runtime audio capture support
-		if (Target.Platform == UnrealTargetPlatform.Win64
-			|| Target.Platform == UnrealTargetPlatform.Win32
+			//this is all for runtime audio capture support
+			if (Target.Platform == UnrealTargetPlatform.Win64
+				|| Target.Platform == UnrealTargetPlatform.Win32
             )
-        {
-            // Add __WINDOWS_WASAPI__ so that RtAudio compiles with WASAPI
-            PublicDefinitions.Add("__WINDOWS_DS__");
+			{
+				// Add __WINDOWS_WASAPI__ so that RtAudio compiles with WASAPI
+				PublicDefinitions.Add("__WINDOWS_DS__");
 
-            // Allow us to use direct sound
-            AddEngineThirdPartyPrivateStaticDependencies(Target, "DirectSound");
+				// Allow us to use direct sound
+				AddEngineThirdPartyPrivateStaticDependencies(Target, "DirectSound");
 			
-			string DirectXSDKDir = Target.UEThirdPartySourceDirectory + "Windows/DirectX";
-			PublicSystemIncludePaths.Add( DirectXSDKDir + "/include");
+				string DirectXSDKDir = Target.UEThirdPartySourceDirectory + "Windows/DirectX";
+				PublicSystemIncludePaths.Add( DirectXSDKDir + "/include");
 
-			PublicAdditionalLibraries.AddRange(
-					new string[] {
-					"dxguid.lib",
-					"dsound.lib"
-					}
-				);
+				PublicAdditionalLibraries.AddRange(
+						new string[] {
+						"dxguid.lib",
+						"dsound.lib"
+						}
+					);
 			}
 		}
 
@@ -225,6 +155,101 @@ namespace UnrealBuildTool.Rules
         {
 			PublicDefinitions.Add("WAVEVR_EYETRACKING");
 			PublicDependencyModuleNames.Add("WaveVR");
+		}
+
+		void Varjo()
+        {
+			if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory, "Varjo")))
+			{
+				System.Console.WriteLine("Cognitive3D.Build.cs found Varjo Plugin folder");
+				PublicDependencyModuleNames.Add("VarjoHMD");
+				PublicDependencyModuleNames.Add("VarjoEyeTracker");
+			}
+		}
+
+		void TobiiEyeTracking()
+        {
+			if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory, "TobiiEyeTracking")))
+			{
+				System.Console.WriteLine("Cognitive3D.Build.cs found TobiiEyeTracking Plugin folder");
+				PrivateIncludePaths.AddRange(
+					new string[] {
+					"../../TobiiEyeTracking/Source/TobiiCore/Private",
+					"../../TobiiEyeTracking/Source/TobiiCore/Public"
+					});
+
+				PublicDependencyModuleNames.Add("TobiiCore");
+			}
+		}
+
+		void SRapnipalVivePro()
+        {
+			if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory, "SRanipal")))
+			{
+				//ideally read the uplugin file as json and get the VersionName
+				//for now, just read the source directory layout
+				var sranipalPlugin = System.IO.Path.Combine(pluginsDirectory, "SRanipal");
+				var sranipalSource = System.IO.Path.Combine(sranipalPlugin, "Source");
+				string[] sourceModules = System.IO.Directory.GetDirectories(sranipalSource);
+				if (sourceModules.Length == 2)//1.1.0.1 and 1.2.0.1 only have eye and lip modules
+				{
+					PublicDefinitions.Add("SRANIPAL_1_2_API");
+					System.Console.WriteLine("Cognitive3D.Build.cs found SRanipal Plugin folder");
+					PrivateIncludePaths.AddRange(
+						new string[] {
+						"../../SRanipal/Source/SRanipal/Private",
+						"../../SRanipal/Source/SRanipal/Public"
+						});
+
+					PublicDependencyModuleNames.Add("SRanipal");
+
+					string BaseDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(ModuleDirectory, "..", "..", ".."));
+					string SRanipalDir = System.IO.Path.Combine(BaseDirectory, "SRanipal", "Binaries", Target.Platform.ToString());
+					PublicAdditionalLibraries.Add(System.IO.Path.Combine(SRanipalDir, "SRanipal.lib"));
+					PublicDelayLoadDLLs.Add(System.IO.Path.Combine(SRanipalDir, "SRanipal.dll"));
+				}
+				else if (sourceModules.Length == 5)
+				{
+					PublicDefinitions.Add("SRANIPAL_1_3_API");
+					System.Console.WriteLine("Cognitive3D.Build.cs found SRanipal Plugin folder 1.3.0.9 or newer");
+					PrivateIncludePaths.AddRange(
+						new string[] {
+						"../../SRanipal/Source/SRanipal/Private",
+						"../../SRanipal/Source/SRanipal/Public",
+						"../../SRanipal/Source/SRanipalEye/Private",
+						"../../SRanipal/Source/SRanipalEye/Public",
+						"../../SRanipal/Source/SRanipalEyeTracker/Private",
+						"../../SRanipal/Source/SRanipalEyeTracker/Public"
+						});
+
+					PublicDependencyModuleNames.Add("SRanipal");
+					PublicDependencyModuleNames.Add("SRanipalEye");
+					PublicDependencyModuleNames.Add("SRanipalEyeTracker");
+
+					string BaseDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(ModuleDirectory, "..", "..", ".."));
+					string SRanipalDir = System.IO.Path.Combine(BaseDirectory, "SRanipal", "Binaries", Target.Platform.ToString());
+					PublicAdditionalLibraries.Add(System.IO.Path.Combine(SRanipalDir, "SRanipal.lib"));
+					PublicDelayLoadDLLs.Add(System.IO.Path.Combine(SRanipalDir, "SRanipal.dll"));
+				}
+			}
+		}
+
+		void PicoMobile()
+        {
+			if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory, "PicoMobile")))
+			{
+				System.Console.WriteLine("Cognitive3D.Build.cs found Pico Plugin folder");
+				PublicDependencyModuleNames.Add("PicoMobile");
+			}
+		}
+
+		void HPGlia()
+        {
+			if (System.IO.Directory.Exists(System.IO.Path.Combine(pluginsDirectory, "HPGlia")))
+			{
+				System.Console.WriteLine("Cognitive3D.Build.cs found HP Glia Omnicept folder");
+				PublicDependencyModuleNames.Add("HPGlia");
+			}
 		}
 	}
 }
