4f709452ca43a53ee49bd5ab36983531568850c0
Alfred Shaker <ashaker@kent.edu>
2025-06-13 16:28:24 -0400

    fix c3d project settings not saving to c3d ini file

--- Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Private/Cognitive3DSettings.cpp
+++ Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Private/Cognitive3DSettings.cpp
@@ -15,12 +15,44 @@ UCognitive3DSettings::UCognitive3DSettings(const FObjectInitializer& ObjectIniti
 
 void UCognitive3DSettings::ReadConfigSettings()
 {
+	const FString CustomConfigFilePath = GetIniName();
 
+	GConfig->GetBool(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("EnableLogging"), EnableLogging, CustomConfigFilePath);
+	GConfig->GetString(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("Gateway"), Gateway, CustomConfigFilePath);
+	GConfig->GetBool(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("EnableLocalCache"), EnableLocalCache, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("LocalCacheSize"), LocalCacheSize, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("GazeBatchSize"), GazeBatchSize, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("CustomEventBatchSize"), CustomEventBatchSize, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("CustomEventAutoTimer"), CustomEventAutoTimer, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("DynamicDataLimit"), DynamicDataLimit, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("DynamicAutoTimer"), DynamicAutoTimer, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("SensorDataLimit"), SensorDataLimit, CustomConfigFilePath);
+	GConfig->GetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("SensorAutoTimer"), SensorAutoTimer, CustomConfigFilePath);
 }
 
 void UCognitive3DSettings::WriteConfigSettings()
 {
+	const FString CustomConfigFilePath = GetIniName();
 
+	GConfig->SetBool(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("EnableLogging"), EnableLogging, CustomConfigFilePath);
+	GConfig->SetString(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("Gateway"), *Gateway, CustomConfigFilePath);
+	GConfig->SetBool(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("EnableLocalCache"), EnableLocalCache, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("LocalCacheSize"), LocalCacheSize, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("GazeBatchSize"), GazeBatchSize, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("CustomEventBatchSize"), CustomEventBatchSize, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("CustomEventAutoTimer"), CustomEventAutoTimer, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("DynamicDataLimit"), DynamicDataLimit, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("DynamicAutoTimer"), DynamicAutoTimer, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("SensorDataLimit"), SensorDataLimit, CustomConfigFilePath);
+	GConfig->SetInt(TEXT("/Script/Cognitive3D.Cognitive3DSettings"), TEXT("SensorAutoTimer"), SensorAutoTimer, CustomConfigFilePath);
+	// ... repeat for each setting
+
+	GConfig->Flush(false, CustomConfigFilePath);
+}
+
+FString UCognitive3DSettings::GetIniName() const
+{
+	return FPaths::Combine(FPaths::ProjectConfigDir(), TEXT("c3dlocal/Cognitive3DSettings.ini"));
 }
 
 
