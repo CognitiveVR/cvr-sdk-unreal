5d6dec70f87d9930ba3bcc2371e1d246801830c3
Alfred Shaker <ashaker@kent.edu>
2025-09-19 22:40:39 -0400

    updating version number

--- Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Public/Cognitive3D.h
+++ Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Public/Cognitive3D.h
@@ -14,7 +14,7 @@
 DEFINE_LOG_CATEGORY_STATIC(Cognitive3D_Log, Log, All);
 
 #define Cognitive3D_SDK_NAME "unreal"
-#define Cognitive3D_SDK_VERSION "2.0.0"
+#define Cognitive3D_SDK_VERSION "2.1.0"
 
 class IAnalyticsProvider;
 class FAnalyticsProviderCognitive3D;
